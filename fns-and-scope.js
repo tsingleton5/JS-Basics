368382b9097347e8527328af25d57b82424bac96
tsingleton5 <tjsingleton5@gmail.com>
2016-05-13 12:24:10 -0600

    git test

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,8 +4,9 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+function isTyler() {
 
+}
 //////////////////PROBLEM 2////////////////////
 
 
@@ -53,7 +54,7 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
