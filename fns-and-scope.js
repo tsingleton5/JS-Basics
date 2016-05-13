fa7f1bcb2bddf6f55288b694f1018f842f74bfbd
tsingleton5 <tjsingleton5@gmail.com>
2016-05-13 13:01:26 -0600

    finished

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,8 +4,13 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-function isTyler() {
-
+function isTyler(name) {
+  if(name === "Tyler") {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 //////////////////PROBLEM 2////////////////////
 
@@ -13,8 +18,9 @@ function isTyler() {
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
-
+function getName() {
+  return prompt("What is your name?");
+}
 
 //////////////////PROBLEM 3////////////////////
 
@@ -23,8 +29,9 @@ function isTyler() {
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
-
+function welcome() {
+  alert("Welcome, " + getName());
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -33,7 +40,14 @@ function isTyler() {
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+// Parameter:
+// A parameter is the thing that is passed into a function definition, you can have as many as you want
+// and they are local variables to that function
+// Ex: function askName (name)
+//
+// Argument:
+// Arguments are the things that are passed into the function when it is invoked
+// Ex: askName("Tom");
 
 
 //////////////////PROBLEM 5////////////////////
@@ -53,17 +67,18 @@ function isTyler() {
 
 //Create a function called myName that returns your name
 
-  //Code Here
-
+function myName() {
+  return "Harry Potter";
+}
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName;
 
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName());
 
 //////////////////PROBLEM 7////////////////////
 
@@ -71,10 +86,16 @@ function isTyler() {
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+function outerFn() {
+  return function() {
+    return "Harry Potter";
+  }
+}
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+var innerFn = outerFn();
 
 //Now invoke innerFn.
+
+innerFn();
